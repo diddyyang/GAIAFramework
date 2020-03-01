b8a4769f9144476f6c6e8df4b507d8a9b0fcb90a
diddyyang <diddy@mail.unsee.com.cn>
2020-03-01 13:57:00 +0800

    Initial Commit

---
+++ GAIAFramework/GAIAFramework.h
@@ -0,0 +1,19 @@
+//
+//  GAIAFramework.h
+//  GAIAFramework
+//
+//  Created by 杨铁 on 2020/3/1.
+//  Copyright © 2020 UnSee Corp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GAIAFramework.
+FOUNDATION_EXPORT double GAIAFrameworkVersionNumber;
+
+//! Project version string for GAIAFramework.
+FOUNDATION_EXPORT const unsigned char GAIAFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GAIAFramework/PublicHeader.h>
+
+
---
+++ GAIAFrameworkTests/GAIAFrameworkTests.m
@@ -0,0 +1,37 @@
+//
+//  GAIAFrameworkTests.m
+//  GAIAFrameworkTests
+//
+//  Created by 杨铁 on 2020/3/1.
+//  Copyright © 2020 UnSee Corp. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GAIAFrameworkTests : XCTestCase
+
+@end
+
+@implementation GAIAFrameworkTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
